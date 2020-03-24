ed0ff6fd1fcf16293534c2e616b0ed81e1f4cc5b
junoguksoo <madden.juno@gmail.com>
2020-03-24 10:55:10 -0700

    Updates

---
+++ precache-manifest.42bbdfefd34cf8587ad5c59f67d5dd3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc086b9bfcad2eff28a0f30569f26b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f3dd52f27b4bbee5cdf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "72570db634e6633e263e",
+    "url": "/static/js/2.dc4e33a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc4e33a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f3dd52f27b4bbee5cdf",
+    "url": "/static/js/main.34710dd3.chunk.js"
+  },
+  {
+    "revision": "1648bedf7f4bf489e2d8",
+    "url": "/static/js/runtime-main.b5fe9e92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
